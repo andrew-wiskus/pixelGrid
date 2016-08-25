bc8b923a2f139d598924be7a092e1aa27916a39f
Drew Wiskus <andrewwiskus@gmail.com>
2016-08-24 19:21:32 -0500

    jankyasssystem

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,24 @@
+
+
+// var myApp = angular.module('myModule', [])
+//   .directive('myDirective', function($timeout) {
+//     return {
+//         restrict: 'A',
+//         link: function(scope, element) {
+//             scope.height = element.prop('offsetHeight');
+//             scope.width = element.prop('offsetWidth');
+//         }
+//     };
+//   })
+// ;
+
+
+
+
 var myApp = angular.module("myApp", ["ngRoute"]);
+
+
+
 
 myApp.config(["$routeProvider", function($routeProvider){
   $routeProvider.
@@ -19,14 +39,89 @@ myApp.config(["$routeProvider", function($routeProvider){
       });
 }]);
 
-myApp.controller("catController", ["$scope", function($scope){
-    console.log("Super cats");
+myApp.controller("catController", ["$scope","$document", function($scope, $document){
+
+makeGrid($document, $scope, "gridContainer", 64);
+
+
+
 }]);
 
-myApp.controller("dogController", ["$scope", function($scope){
+myApp.controller("dogController", ["$scope","$document", function($scope, $document){
     console.log("Dogs are neat");
 }]);
 
-myApp.controller("lizardController", ["$scope", function($scope){
+myApp.controller("lizardController", ["$scope","$document", function($scope,$document){
     console.log("Lizards are toxic");
 }]);
+
+//8,32,64 bit.
+function makeGrid($document, $scope, containerID, bitSize){
+
+  var gridContainer = $document[0].getElementById(containerID);
+  var gridHeight = gridContainer.offsetHeight;
+  var gridWidth = gridContainer.offsetWidth;
+
+  var size = gridHeight / bitSize;
+  var pixelArray = [];
+  var pixelAmount = bitSize * bitSize;
+
+  var row = 0;
+  var column = 0;
+  var randomColor = 0;
+  var colorString = "";
+
+  function Pixel(color, yPos, xPos, size, id){
+    this.color = color;
+    this.yPos = yPos;
+    this.xPos = xPos;
+    this.height = size + "px";
+    this.width = size + "px";
+    this.id = id;
+  }
+
+  for(var i = 0; i < pixelAmount; i++){
+    randomColor = getRandomInt(0,999999);
+    column++;
+    if (column == bitSize + 1){
+      column = 1;
+    }
+    if (i % bitSize === 0){
+      row++;
+    }
+
+    // pixel.id = pixel.xPos + "," + pixel.yPos;
+    // pixel.color = "#" + randomColor;
+    colorString = getColorString(randomColor);
+
+    var id = "x" + column + "y" + row;
+    // console.log(pixel);
+    var newPixel = new Pixel(colorString, row, column, size, id);
+    pixelArray.push(newPixel);
+  }
+  console.log(pixelArray);
+  $scope.pixels = pixelArray;
+
+
+  console.log("Grid size: ", gridWidth, "px by ", gridHeight, "px.");
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getColorString(num){
+  num = num + "";
+  var amountOfZero = num.length - 6;
+  var zeros = "";
+
+  if (amountOfZero > 0){
+    for (var i = 0; i < amountOfZero; i++){
+      zeros = zeros + "0";
+    }
+  }
+
+  return zeros + num;
+}
